256faefc7fb9d1bed0b91936cb454ce11119c63b
Новиков Кирилл Сергеевич <novikovkirillsergeevic@MacBook-Air-Novikov.local>
2020-11-18 01:35:01 +0300

    Added useForm hook

--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import Button from '../button';
+import useForm from '../../hooks/use-form';
 
 import styles from './upload.module.css'
 
+const INITIAL_VALUES = { english: '', russian: '' };
+
 const Upload = () => {
-  
+  const { handlers, reset } = useForm(INITIAL_VALUES);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    //onSubmit(values);
+    reset();
+  };
+
   return (
   	<div>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <div>
-          <input className={styles.input}
+          <input className={ styles.input }
             placeholder="Your word in english"
+            { ...handlers.english }
           />
         </div>
         <div>
           <input className={ styles.input }
             placeholder="Translation in russian"
+            { ...handlers.russian }
           />
         </div>
         <div>
